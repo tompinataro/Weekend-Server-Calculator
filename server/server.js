5684bcc9068cd0495f3ecd7dbb96a34e017f3e1c
Tom Pinataro <tompinataro@gmail.com>
2024-07-23 20:38:37 -0500

    port issue

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-let PORT = process.env.PORT || 5000;
+let PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(express.static('server/public'));
